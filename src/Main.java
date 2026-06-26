32c3b48bed229797ffec8a16fa3c18e00fa29daf
sakhile <sakhiledabula0444@gmail.com>
2026-06-26 09:37:58 +0200

    modified

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Account acc = new Account();
-
-        // Deposit
-        System.out.print("Enter deposit amount: ");
-        double depositAmount = scanner.nextDouble();
-        TransactionService.deposit(acc, depositAmount);
-
-        System.out.println("Current balance: " + acc.getBalance());
-
-        // Withdrawal
-        System.out.print("Enter withdrawal amount: ");
-        double withdrawalAmount = scanner.nextDouble();
-        TransactionService.withdraw(acc, withdrawalAmount);
-
-        System.out.println("Final balance: " + acc.getBalance());
-
-        scanner.close();
+        System.out.println("Hello World");
     }
 }
