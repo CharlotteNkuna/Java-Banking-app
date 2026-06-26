c998b4fafab194e6a6aa27f1431e4f4e18d91a38
sakhile <sakhiledabula0444@gmail.com>
2026-06-26 09:24:27 +0200

    updated

---
+++ src/Account.java
@@ -0,0 +1,20 @@
+public class Account {
+
+    private double balance = 0.0;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+}
