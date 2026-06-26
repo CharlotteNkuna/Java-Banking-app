f42c42c7338d6d7c7b1644ff4d2810e10832d7d8
sakhile <sakhiledabula0444@gmail.com>
2026-06-26 11:44:38 +0200

    made changes

--- src/TransactionService.java
+++ src/TransactionService.java
@@ -1,15 +1,30 @@
 public class TransactionService {
 
-    public static void deposit(Account acc, double amount) {
-        acc.deposit(amount);
+    public static void deposit(Account account, double amount) {
+
+        if (amount <= 0) {
+            System.out.println("Invalid deposit amount");
+            return;
+        }
+
+        account.deposit(amount);
+
         System.out.println("Deposit successful");
+        System.out.println("New Balance: R" + account.getBalance());
     }
 
-    public static void withdraw(Account acc, double amount) {
-        if (acc.withdraw(amount)) {
+    public static void withdraw(Account account, double amount) {
+
+        if (amount <= 0) {
+            System.out.println("Invalid withdrawal amount");
+            return;
+        }
+
+        if (account.withdraw(amount)) {
             System.out.println("Withdrawal successful");
+            System.out.println("Remaining Balance: R" + account.getBalance());
         } else {
-            System.out.println("Insufficient balance");
+            System.out.println("Insufficient funds");
         }
     }
 }
